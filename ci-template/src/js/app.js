67074e45475ee1de7d775732a85664a069fa71a3
Marina Legalova <ML1305@yandex.ru>
2025-03-23 18:52:11 +0500

    containers

--- ci-template/src/js/app.js
+++ ci-template/src/js/app.js
@@ -5,17 +5,3 @@ console.log('worked');
 
 console.log(sum([1, 2]));
 
-import showHealth from './health';
- 
-console.log('worked');
- 
-showHealth(70);
-
-import sortUnits from './sort';
-
-const a = [
-  { name: 'мечник', health: 10 },
-  { name: 'маг', health: 100 },
-  { name: 'лучник', health: 80 },
-];
-sortUnits(a);
